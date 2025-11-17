9f39d99c236db418ac8875597ffa6b56bf540a07
s.tayou <stevetayou@gmail.com>
2025-11-17 13:22:14 +0100

    Categories Menu

---
+++ app/components/desktop/categoriesMenuItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {computed, watch} from "vue";
+
+  const props = defineProps({
+    active: { type: Boolean },
+    iconSrc: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  })
+
+  const classes = computed(() => props.active ? ["active"] : [])
+</script>
+
+<template>
+<div :class="classes" class="categories-menu-item font-medium">
+  <img :src="iconSrc" alt="icon">
+  <span>{{ name }}</span>
+</div>
+</template>
+
+<style scoped>
+@layer modules {
+  .categories-menu-item {
+    padding: 1rem 2rem;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    border-inline-start: 2px solid transparent;
+    cursor: pointer;
+
+    &:hover, &.active {
+      border-color: var(--primary-color);
+      background-color: var(--light-grey);
+    }
+  }
+}
+</style>
---
+++ app/components/desktop/categoriesMenuItemsDetail.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+  defineProps({
+    items: Array,
+  })
+</script>
+
+<template>
+  <div class="categories-menu-items-detail">
+    <div v-for="item in items" :key="item.group">
+      <span class="categories-menu-items-detail--group">{{ item.group }}</span>
+      <ul>
+        <li class="categories-menu-items-detail--item font-regular" v-for="cat in item.items">
+          <nuxt-link href="#">{{ cat }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@layer modules {
+  .categories-menu-items-detail {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 1.5rem;
+    grid-auto-flow: dense;
+    padding: var(--default-padding);
+    position: absolute;
+    left: 100%;
+    top: -1px;
+    width: 50rem;
+    background-color: #FFF;
+    border: 1px solid var(--grey-border-color);
+    box-shadow: var(--light-grey) -1px 0 1px;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      gap: .875rem;
+
+      ul {
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+      }
+    }
+  }
+  .categories-menu-items-detail--item {
+    font-size: .875rem;
+    color: var(--grey-font-color);
+
+    a:hover {
+      color: var(--font-black-color);
+    }
+  }
+}
+</style>
